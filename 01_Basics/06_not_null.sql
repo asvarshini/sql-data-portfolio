36a2a97c7e00c19dc5beda82eac66758d9d81320
Varshini A S <asvarshini84@gmail.com>
2026-06-29 17:44:24 +0530

    Update 06_not_null.sql

--- 01_Basics/06_not_null.sql
+++ 01_Basics/06_not_null.sql
@@ -1,3 +1,9 @@
+NULL represents a missing or unknown value in a database. 
+It is different from 0 or an empty string (''). 
+Using NULL helps avoid incorrect calculations (such as averages) when data is unavailable. 
+To check for NULL values in SQL, use IS NULL or IS NOT NULL instead of =.
+
+    
 CREATE TABLE department (
     emp_id INT NOT NULL,
     emp_name VARCHAR(50) NOT NULL
@@ -13,3 +19,5 @@ CREATE TABLE department (
 INSERT INTO department VALUES (1, 'Varshini');
 
 SELECT * FROM department;
+-- How do we check for NULL?
+WHERE Name IS NULL
